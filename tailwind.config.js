f20894674cab9c68b176926a9fe98f5b0f73c7a5
GabHurlant <gabin.lasne@gmail.com>
2024-12-26 10:52:05 +0100

    ajout compilation front dans le make file et léger changements de style

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBrown: '#af864a',
+      },
+    },
   },
   plugins: [],
 }
